29cbcb9063204354ade21541a3857cb0e5c990f0
Nesty04 <barkhotkina04@mail.ru>
2024-06-07 21:31:51 +0300

    corrected

--- Lab_8.py
+++ Lab_8.py
@@ -1,6 +1,11 @@
 #Бархоткина Анастасия, 408226
 import cv2
 
+start_position = 2
+left = 0
+right = 0
+
+
 def image_resizing():
     img = cv2.imread('images/variant-6.png')
     resized_img = cv2.resize(img, None, fx=2, fy=2, interpolation=cv2.INTER_LINEAR)
@@ -18,20 +23,23 @@ def point_detecting():
             break
         mask = cv2.inRange(image, (80, 80, 80), (255, 255, 255))
         ret, thresh = cv2.threshold(mask, 110, 255, cv2.THRESH_BINARY_INV)
-        contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        contours, hierarchy = cv2.findContours(thresh, 
+                                               cv2.RETR_EXTERNAL, 
+                                               cv2.CHAIN_APPROX_NONE
+                                               )
         if len(contours) > 0:
             c = max(contours, key=cv2.contourArea)
             x, y, w, h = cv2.boundingRect(c)
-            cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
         cv2.imshow("image", image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
 
 def side_counting():
-    OLDposition = 2
-    Left = 0
-    Right = 0
+    global start_position
+    global left
+    global right
     cap = cv2.VideoCapture("sample.mp4")
     while (cv2.waitKey(1) != 27):
         ret, image = cap.read()
@@ -39,50 +47,62 @@ def side_counting():
             break
         mask=cv2.inRange(image, (80, 80, 80), (255, 255, 255))
         ret, thresh = cv2.threshold(mask, 110, 255, cv2.THRESH_BINARY_INV)
-        contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        contours, hierarchy = cv2.findContours(thresh, 
+                                               cv2.RETR_EXTERNAL, 
+                                               cv2.CHAIN_APPROX_NONE
+                                               )
         if len(contours) > 0:
             c = max(contours, key=cv2.contourArea)
             x, y, w, h = cv2.boundingRect(c)
-            cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        centreX = image.shape[1]//2
-        if x > (centreX):
-            NEWposition = 2
-        if x + w < (centreX):
-            NEWposition = 1
-        if NEWposition != OLDposition:
-            if OLDposition == 2:
-                Left += 1
-            if OLDposition == 1:
-                Right += 1
-            OLDposition = NEWposition
-        cv2.putText(image,  str(Left), (100,100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
-        cv2.putText(image,  str(Right), (500,100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        centre_x = image.shape[1] // 2
+        if x > (centre_x):
+            final_position = 2
+        if x + w < (centre_x):
+            final_position = 1
+        if final_position != start_position:
+            if start_position == 2:
+                left += 1
+            if start_position == 1:
+                right += 1
+            start_position = final_position
+        cv2.putText(image,  
+                    str(left), (100, 100), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, 
+                    (255, 0, 0), 2)
+        cv2.putText(image,
+                    str(right), (500, 100), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, 
+                    (255, 0, 0), 2)
         cv2.imshow("image", image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
 
 def fly_detecting():
-    OLDposition = 2
-    Left = 0
-    Right = 0
+    start_position = 2
+    left = 0
+    right = 0
     cap = cv2.VideoCapture("sample.mp4")
-    while (cv2.waitKey(1) != 27):
+    while cv2.waitKey(1) != 27:
         ret, image = cap.read()
         if not ret:
             break
         mask=cv2.inRange(image, (80, 80, 80), (255, 255, 255))
         ret, thresh = cv2.threshold(mask, 110, 255, cv2.THRESH_BINARY_INV)
-        contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        contours, hierarchy = cv2.findContours(thresh, 
+                                               cv2.RETR_EXTERNAL, 
+                                               cv2.CHAIN_APPROX_NONE
+                                               )
         if len(contours) > 0:
             c = max(contours, key=cv2.contourArea)
             x, y, w, h = cv2.boundingRect(c)
-            cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
         a = x + (w // 2)
         b = y + (h // 2)
         #print(a, b)
         fly = cv2.imread("fly64.png")
-        fly = cv2.resize(fly,(64,64))
+        fly = cv2.resize(fly,(64, 64))
         for i in range(64):
             for j in range(64):
                 dx = (a - 32 + j)
@@ -96,19 +116,23 @@ def fly_detecting():
                 if dy > image.shape[0] - 1:
                     dy = image.shape[0] - 1
                 image[dy][dx] = fly[j][i]
-        centreX = image.shape[1]//2
-        if x>(centreX):
-            NEWposition = 2
-        if x+w<(centreX):
-            NEWposition = 1
-        if NEWposition != OLDposition:
-            if OLDposition == 2:
-                Left += 1
-            if OLDposition == 1:
-                Right += 1
-            OLDposition=NEWposition
-        cv2.putText(image,  str(Left), (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0) , 2)
-        cv2.putText(image,  str(Right), (500, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0) , 2)
+        centre_x = image.shape[1] // 2
+        if x > (centre_x):
+            finish_position = 2
+        if (x + w) < (centre_x):
+            finish_position = 1
+        if finish_position != start_position:
+            if start_position == 2:
+                left += 1
+            if start_position == 1:
+                right += 1
+            start_position = finish_position
+        cv2.putText(image,  
+                    str(left), (100, 100), cv2.FONT_HERSHEY_SIMPLEX,
+                    1, (255, 0, 0) , 2)
+        cv2.putText(image,  
+                    str(right), (500, 100), cv2.FONT_HERSHEY_SIMPLEX,
+                    1, (255, 0, 0) , 2)
         cv2.imshow("image", image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
@@ -119,5 +143,3 @@ if __name__ == '__main__':
     point_detecting()
     side_counting()
     fly_detecting()
-    
-    
